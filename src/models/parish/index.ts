ea33f85d9a33a313413b695f0e820a105aa21a78
NikDark <nikefremov.00.008@hotmail.com>
2023-01-10 01:12:30 +0300

    Hot-Fix: Fix limit response from parish filter

--- src/models/parish/index.ts
+++ src/models/parish/index.ts
@@ -14,6 +14,7 @@ import { Filters, Parish } from './types';
 const { REACT_APP_API_URL } = process.env;
 
 const DATE_MASK = 'dd-MM-yyyy HH:mm:ss';
+const LIMIT = 100;
 
 export const ParishGate = createGate();
 export const $parishes = createStore<Parish[]>([]);
@@ -40,6 +41,7 @@ export const fetchParishesFiltersFx = createEffect(async (filters: Filters) => {
   const res = await axios.get(`${REACT_APP_API_URL}parish`, {
     params: {
       filter: Object.entries(filters).map(([key, value]) => new Array<string | undefined>().concat(value).map((data) => `${key}==${data}`)),
+      limit: LIMIT,
     },
     paramsSerializer(params) {
       return Qs.stringify(params, { indices: false });
