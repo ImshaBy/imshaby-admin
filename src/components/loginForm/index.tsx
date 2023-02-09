03cca87908762d0b2591612bcea28c14fe603cb6
Mikita Yafremau <50526884+NikDark@users.noreply.github.com>
2023-02-09 09:33:17 +0300

    Task/design admin login page (#92)
    * Create base TextField for project
    
    * Add toggle for header tabs
    
    * Fit width for mobile snackbar
    
    * Add new design for login form
    
    * Fix min-height of header

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,30 +1,60 @@
 import './style.scss';
 
+import { createTheme, TextField as MuiTextField, ThemeProvider } from '@mui/material';
 import React from 'react';
 
+import useWindowSize from '../../utils/useWindowSize';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(183, 32, 37)',
+    },
+  },
+});
+
 interface Props {
-  onSubmit: (event: any) => void,
+  onSubmit: (event: any) => void;
 }
 
-const LoginForm = ({
-  onSubmit,
-}: Props) => (
-  <div className="login_form_container">
-    <form className="login" data-bitwarden-watching="1" onSubmit={onSubmit}>
-      <span className="login__title">
-        Account Login
-      </span>
-      <div className="email" data-validate="Valid email is required: ex@abc.xyz">
-        <input className="email__input" type="text" name="email" required placeholder="Email" />
-      </div>
+const LoginForm = ({ onSubmit }: Props) => {
+  const [width] = useWindowSize();
+  const isMobile = width <= 767;
 
-      <div className="login__signin">
-        <button type="submit" className="login__signin__btn">
-          Sign in
-        </button>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="login_form_container">
+        <form spellCheck="false" className="login" data-bitwarden-watching="1" onSubmit={onSubmit}>
+          <span className="login__title">Уваход</span>
+          <span className="login__subtitle">
+            Уваход у панэль кіравання раскладам Імш парафіі адбываецца праз электронную пошту
+          </span>
+          <div className="email" data-validate="Valid email is required: ex@abc.xyz">
+            <MuiTextField
+              fullWidth
+              className="email__input"
+              variant="standard"
+              name="email"
+              placeholder="Электронная пошта"
+            />
+          </div>
+          <div className="login__signin">
+            <button type="submit" className="login__signin__btn btn">
+              Выслаць спасылку на
+              {' '}
+              {isMobile ? 'эл. пошту' : 'электронную пошту'}
+            </button>
+            <span className="login__signin__help">
+              Падтрымка:&nbsp;
+              <a href="mailto:byimsha@gmail.com">
+                byimsha@gmail.com
+              </a>
+            </span>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-);
+    </ThemeProvider>
+  );
+};
 
 export default LoginForm;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,6 @@ import { useToasts } from 'react-toast-notifications';
 import Header from '../components/header';
 import LoginForm from '../components/loginForm';
 import Section from '../components/section';
-import SectionHeader from '../components/sectionHeader';
 import { LoginGate } from '../models/app';
 import Auth from '../utils/auth';
 
@@ -23,7 +22,7 @@ const LoginPage = () => {
     // Send magic link to the provided email
     try {
       await auth.sendMagicLink(event.target.email.value);
-      setMsg('Спасылка адправлена на пошту');
+      setMsg('Спасылка на ўваход адпраўлена на электронную пошту');
     } catch (err) {
       if (err instanceof Error) {
         setMsg(err.message);
@@ -39,17 +38,21 @@ const LoginPage = () => {
 
   return (
     <>
-      <Header />
-      <Section
-        header={
-          <SectionHeader title="Login Page" />
-        }
-        content={(
-          <>
-            <LoginForm onSubmit={handleSubmit} />
-          </>
-        )}
+      <Header
+        schedule={false}
+        parish={false}
+        select={false}
       />
+      <div style={{ marginTop: 50 }}>
+        <Section
+          header={<></>}
+          content={(
+            <>
+              <LoginForm onSubmit={handleSubmit} />
+            </>
+          )}
+        />
+      </div>
     </>
   );
 };
