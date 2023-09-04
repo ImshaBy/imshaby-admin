98346000658093e37d9781681c1b9d42acc3163f
NikDark <nikefremov.00.008@hotmail.com>
2023-09-04 23:34:55 +0300

    Add react-imask

--- src/components/timepicker/index.tsx
+++ src/components/timepicker/index.tsx
@@ -4,6 +4,8 @@ import { TextField } from '@mui/material';
 import moment from 'moment';
 import React, { ChangeEvent, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { IMask, IMaskInput } from 'react-imask';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import TimeField from 'react-simple-timefield';
 
 interface props {
@@ -12,10 +14,52 @@ interface props {
   onChange: (hour: string, minute: string) => void;
 }
 
+interface CustomProps {
+  onChange: (event: ChangeEvent<HTMLInputElement>, value: moment.Moment) => void;
+}
+
+const TextMaskCustom = React.forwardRef<HTMLInputElement, CustomProps>(
+  (props, ref) => {
+    const { onChange, ...other } = props;
+    const onAccept = (value: string) => {
+      const newValue = moment(value, 'HH:mm', true);
+      if (newValue.isValid()) {
+        onChange(newValue);
+      }
+    };
+    return (
+      <IMaskInput
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...other}
+        mask={Date}
+        pattern="HH:MM"
+        blocks={{
+          HH: {
+            mask: new IMask.MaskedRange({ from: 0, to: 23, lazy: false }),
+          },
+          MM: {
+            mask: new IMask.MaskedRange({ from: 0, to: 59, lazy: false }),
+          },
+        }}
+        inputRef={ref}
+        // eslint-disable-next-line max-len
+        onAccept={onAccept}
+        overwrite
+        lazy={false}
+        // define date -> str convertion
+        format={(date: any) => moment(date).format('HH:mm')}
+        // define str -> date convertion
+        parse={(str: string) => moment(str, 'HH:mm').toDate()}
+      />
+    );
+  },
+);
+
+
 const TimePicker = ({ hour, minute, onChange }: props) => {
   // const [active, setActive] = useState(false);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>, value: string) => {
+  const handleChange = (value: string) => {
     const newTime = moment(value, 'HH:mm');
     if (newTime.isValid()) {
       onChange(
@@ -25,28 +69,16 @@ const TimePicker = ({ hour, minute, onChange }: props) => {
     }
   };
 
-  // const handleFocus = (event: ChangeEvent) => {
-  //   setActive(true);
-  // };
-
-  const inputComponent = (
-    <TextField
-      placeholder="09:00"
-      className="timePicker__field"
-      color="error"
-      InputProps={{
-        className: 'timePicker__field__input',
-      }}
-      // onFocus={handleFocus}
-    />
-  );
-
   return (
     <section className="timePicker">
-      <TimeField
-        value={moment(`${hour || 0}:${minute || 0}`, 'HH:mm').format('HH:mm')}
+      <TextField
+        className="timePicker__field"
+        // color="error"
+        InputProps={{
+          className: 'timePicker__field__input',
+          inputComponent: TextMaskCustom as any,
+        }}
         onChange={handleChange}
-        input={inputComponent}
       />
     </section>
   );
