b49f6e95a6d880f8c0c7ef1f18fabb40c5947025
INovysh <77119308+INovysh@users.noreply.github.com>
2024-08-12 17:52:41 +0300

    fix: add x-date-pickers for time (#127)

--- src/components/timepicker/index.tsx
+++ src/components/timepicker/index.tsx
@@ -1,10 +1,9 @@
 import './style.scss';
 
-import { TextField } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopTimePicker as MuiTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import moment from 'moment';
-import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { IMask, IMaskInput } from 'react-imask';
 
 interface props {
   hour?: string | null;
@@ -12,78 +11,29 @@ interface props {
   onChange: (hour: string, minute: string) => void;
 }
 
-interface CustomProps {
-  onChange: (value: string) => void;
-}
-
-const TextMaskCustom = React.forwardRef<HTMLInputElement, CustomProps>(
-  (props, ref) => {
-    const { onChange, ...other } = props;
-    const onAccept = (value: string) => {
-      const newValue = moment(value, 'HH:mm', true);
-      if (newValue.isValid()) {
-        onChange(value);
-      }
-    };
-    return (
-      <IMaskInput
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...other}
-        mask={Date}
-        pattern="`HH:`MM"
-        blocks={{
-          HH: {
-            mask: IMask.MaskedRange,
-            from: 0,
-            to: 23,
-            lazy: false,
-            eager: true,
-          },
-          MM: {
-            mask: IMask.MaskedRange,
-            from: 0,
-            to: 59,
-            lazy: false,
-            eager: true,
-          }
-        }}
-        inputRef={ref}
-        // eslint-disable-next-line max-len
-        onAccept={onAccept}
-        lazy={false}
-        eager
-        // define date -> str convertion
-        format={(date: any) => moment(date).format('HH:mm')}
-        // define str -> date convertion
-        parse={(str: string) => moment(str, 'HH:mm').toDate()}
-      />
-    );
-  },
-);
-
 const TimePicker = ({ hour, minute, onChange }: props) => {
   const handleChange = (value: string) => {
     const newTime = moment(value, 'HH:mm');
     if (newTime.isValid()) {
-      onChange(
-        (`0${newTime.hour()}`).slice(-2),
-        (`0${newTime.minute()}`).slice(-2),
-      );
+      onChange(`0${newTime.hour()}`.slice(-2), `0${newTime.minute()}`.slice(-2));
     }
   };
 
   return (
     <section className="timePicker">
-      <TextField
-        className="timePicker__field"
-        value={moment(`${hour}:${minute}`, 'HH:mm').format('HH:mm')}
-        InputProps={{
-          className: 'timePicker__field__input',
-          inputComponent: TextMaskCustom as any,
-        }}
-        color="error"
-        onChange={(value) => handleChange(value as any)}
-      />
+      <LocalizationProvider
+        dateAdapter={AdapterMoment}
+        localeText={{ fieldHoursPlaceholder: () => '__', fieldMinutesPlaceholder: () => '__' }}
+      >
+        <MuiTimePicker
+          value={moment(`${hour}:${minute}`, 'HH:mm')}
+          onChange={(value) => {
+            handleChange(value as any);
+          }}
+          disableOpenPicker
+          ampm={false}
+        />
+      </LocalizationProvider>
     </section>
   );
 };
